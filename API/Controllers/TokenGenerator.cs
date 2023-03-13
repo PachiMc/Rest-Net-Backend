e668c1bdef00b04fec31e3442361729076ecd278
Pachi <pachimc@outlook.com.ar>
2023-03-12 21:10:39 -0300

    fixs

--- API/Controllers/TokenGenerator.cs
+++ API/Controllers/TokenGenerator.cs
@@ -3,7 +3,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using System.Security.Principal;
 
 namespace API.Controllers
 {
